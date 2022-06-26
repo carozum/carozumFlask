24af57a7272f2a11dcc90520db9d2a24a7fc0a5a
carozum <czumbiehlfaure@hotmail.fr>
2022-06-26 15:00:41 +0200

    first flask app with guess number game v 1.4

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from re import M
-from venv import create
 from flask import Flask
 
 # app is an object, declared with the flask class, __name__ is one of its attributes. 
 app = Flask(__name__)
 
+#*******************************************************
 # decorators
+#*******************************************************
 
 def make_bold(function):
     def wrapper_function():
@@ -21,6 +21,10 @@ def make_underline(function):
     def wrapper():
         return f"<u>{function()}</u>"
     return wrapper
+
+#*******************************************************
+# page home
+#*******************************************************
     
 # url '/' is the home page. route() is a decorator function, a method which lives inside the app object and which adds a functionality to the function that is passed inside it. @ is used to simplify the code (shortcut) 
 @app.route('/')
@@ -29,6 +33,10 @@ def make_underline(function):
 @make_underline
 def home():
     return 'Hello, World!'
+
+#*******************************************************
+# page contact
+#*******************************************************
 
 @app.route('/contact')
 def contact():
@@ -39,6 +47,10 @@ def contact():
     '''
     return content
 
+#*******************************************************
+# page user name
+#*******************************************************
+
 # extract parts that we want variable rules : you can add variable sections to a URL by making sections with <variavle_name>. Your function then receives the <variable_name> as a keyword argument. Optionnaly you can use a converter to specify the type of the argument like <converter: variable_name>. converted bu default to a string. <path:angela> <int: 31>
 # I can search for /username/caro
 # attention for each update in the path @app.route('...') I have to stop the server and rerun it so that changes will be taken into account -> there is a solution to that inconvenient : debug mode on (= automatic reloader + debug on)
@@ -46,6 +58,32 @@ def contact():
 def greet(name, age):
     return f"hello {name}. You are {age} years old"
 
+#*******************************************************
+# page random number
+#*******************************************************
+
+from random import randint
+numbertoguess = randint(0, 9)
+
+@app.route('/guessanumber/<int:number>')
+def guess(number):
+    answer = f'''<div style="text-align: center">
+            <iframe src="https://giphy.com/embed/xUn3CftPBajoflzROU" width="100" height="100" frameBorder="0" class="giphy-embed" allowFullScreen ></iframe>
+            <h1>Guess a number between 0 and 9</h1>
+            <p>You entered {number}</p>
+            <p>Number to guess is {numbertoguess}</p>
+            <div>'''
+    if number < numbertoguess:
+        answer += '<p style="color: pink; font-size: 3rem">The number entered is too low</p>' 
+    elif number > numbertoguess:
+        answer += '<p style="color: blue; font-size: 3rem">The number entered is too high</p>'
+    else:
+        answer += '''<p style="color: green; font-size: 3rem">You found me !</p><iframe src="https://giphy.com/embed/3ohzdIuqJoo8QdKlnW" width="480" height="222" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'''
+    return answer
+
+#*******************************************************
+# launch the server
+#*******************************************************
 
 # to simplify server set up and launch. 
 # file server will now be run in debug Mode - flask debugger
